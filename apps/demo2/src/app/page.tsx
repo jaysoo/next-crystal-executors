1632238a076af5b586f60236af0d7ef5fc0f543e
Jack Hsu <jack.hsu@gmail.com>
2024-02-27 12:44:57 -0500

    added second app using executors

---
+++ apps/demo2/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Ui } from '@next3/ui'
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return <Ui/>;
+}
